8b0daea6149d85ebe0038c9ec9b4fbf11621b413
Shashank Goyal <shashankgoyal095@gmail.com>
2024-03-13 20:22:11 -0400

    add allowlist to adming

--- correlate/users/admin.py
+++ correlate/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import User
-from .models import WatchList
+from .models import User, WatchList, Allowlist
 
 admin.site.register(User)
 admin.site.register(WatchList)
+admin.site.register(Allowlist)
