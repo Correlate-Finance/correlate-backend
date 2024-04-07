4e438634692bdc856958a508d89b58359f01ed88
Zaryab Ahmad <zaryabahmad@Zaryabs-MacBook-Air.local>
2024-04-07 05:28:33 +0500

    Minor bug fix

--- correlate/users/emails.py
+++ correlate/users/emails.py
@@ -7,7 +7,7 @@ def send_otp_via_email(email):
     subject = 'Your OTP for password reset'
     otp = random.randint(100000, 999999)
     message = f'Your OTP is {otp}'
-    email_from = settings.EMAIL_HOST
+    email_from = settings.EMAIL_HOST_USER
     send_mail(subject, message, email_from, [email])
     user = User.objects.get(email=email)
     user.otp = otp
